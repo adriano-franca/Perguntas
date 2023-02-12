d172975b3c055f9f8b1e0381d182935474e28b0c
adrianofranca111 <adriano.franca@discente.ufma.br>
2023-02-11 22:03:21 -0300

    alterações do aprendizado 2

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-    var nome = "Adriano";
-    var lang = "React Native"
-    res.render("index", {nome: nome, lang: lang, empresa: "PETComp", participantes: 12});
+app.get("/:nome/:lang", (req, res) => {
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var exibirMsg = true;
+    res.render("index", {nome: nome,
+        lang: lang,
+        empresa: "PETComp",
+        participantes: 12,
+        msg: exibirMsg
+    });
 });
 
 
